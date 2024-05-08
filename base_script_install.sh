253f011fbaab8dd65cc785985e0b02b86e08c431
prioritize <jared@prototypic.al>
2024-05-07 23:18:55 -0700

    Initial commit

---
+++ base_script_install.sh
@@ -0,0 +1,15 @@
+#! /bin/zsh
+Install zsh
+Install tmux
+Install rustup
+Install alacritty
+Install ripgrep
+Install git
+Install nvim
+Clone kickvim
+Clone tmux_conf
+Clone alacritty_conf
+Clone zsh_conf
+Install gcc-9
+Install g++-9
+
